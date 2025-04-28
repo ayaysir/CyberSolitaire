238a5b161f37c84ed4b2096c618df918471ad6b3
윤범태 <yoonbumtae@gmail.com>
2025-04-28 17:25:53 +0900

    Initial Commit

---
+++ CyberSolitaire/CyberSolitaireApp.swift
@@ -0,0 +1,20 @@
+//
+//  CyberSolitaireApp.swift
+//  CyberSolitaire
+//
+//  Created by 윤범태 on 4/28/25.
+//
+
+import SwiftUI
+
+@main
+struct CyberSolitaireApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
